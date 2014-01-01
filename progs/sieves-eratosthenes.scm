615cab21941a640d4db3dc1a9d7cb5063be6e9b4
Mario Domenech Goulart <mario.goulart@gmail.com>
2013-12-31 23:57:25 -0200

    Add progs/sieves-eratosthenes.scm

---
+++ progs/sieves-eratosthenes.scm
@@ -0,0 +1,20 @@
+;; From http://www.reddit.com/r/scheme/comments/1u2ac6/scheme_interpreter_speed_comparison/
+
+(define (primes<2n n)
+  (erato 2 (* n 2) (list)))
+
+(define (erato n z primes)
+
+  (define (sieve s)
+    (if (or (null? s)                     ; prime
+            (< n (* (car s) (car s))))    ; prime!
+        (erato (+ n 1) z (append primes (list n)))
+        (if (zero? (modulo n (car s)))  ; composite
+            (erato (+ n 1) z primes)
+            (sieve (cdr s)))))
+
+  (if (< n z)
+      (sieve primes)
+      primes))
+
+(time (primes<2n 60000))
