mario-goulart/chicken-benchmarks

commit bf374b9e08a79128b90363d7e369cceb76f11675

run.scm: wrap into a module

run.scm
@@ -3,7 +3,11 @@
 exec csi -s $0 "$@"
 |#
 
-(use posix extras utils files data-structures srfi-1 srfi-13 irregex)
+(module run-benchmarks ()
+
+(import chicken scheme)
+
+(use data-structures extras files posix utils srfi-1 srfi-13 irregex)
 (use (only setup-api program-path))
 
 ;;; Configurable parameters
@@ -210,3 +214,5 @@ exec csi -s $0 "$@"
     (run-all)
     (print "\nTotal run time: "
            (prettify-time (- (current-seconds) start)))))
+
+) ;; end module

commit 0ecc20f78475a8c086b2498b76c87edf7a158af9

run.scm: remove module declaration
With the module declaration, the loaded configuration file cannot
access the symbols wrapped into the module.

Issue introduced by bf374b9e08a79128b90363d7e369cceb76f11675

Use "csc -ASM run.scm" to check the code with the scrutinizer.

run.scm
@@ -3,8 +3,6 @@
 exec csi -s $0 "$@"
 |#
 
-(module run-benchmarks ()
-
 (import chicken scheme)
 
 (use data-structures extras files posix utils srfi-1 srfi-13 irregex)
@@ -466,5 +464,3 @@ Total number of major GCs       #(alist-ref 'major-gcs counts)
 Total number failures           #(alist-ref 'failures counts)
 EOF
 )))
-
-) ;; end module