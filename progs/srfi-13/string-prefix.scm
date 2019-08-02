e2266df9a95250094dbe79f0a8111ab0191ba7eb
Mario Domenech Goulart <mario@parenteses.org>
2019-08-02 17:53:59 +0200

    progs/srfi-13: support both C4 and C5
    Thanks to Peter Bex for spotting the lack of support for C5.
    
    Used
    
    sed 's/(use srfi-13)/(cond-expand\
     (chicken-4 (use srfi-13))\
     (chicken-5 (import srfi-13))\
     (else (error "Unsupported CHICKEN version.")))/' *.scm -i
    
    to implement this change.

--- progs/srfi-13/string-prefix.scm
+++ progs/srfi-13/string-prefix.scm
@@ -1,4 +1,7 @@
-(use srfi-13)
+(cond-expand
+ (chicken-4 (use srfi-13))
+ (chicken-5 (import srfi-13))
+ (else (error "Unsupported CHICKEN version.")))
 (include "utils/utils.scm")
 
 (time (repeat 1000000 (string-prefix? "foo" "askkkkkkkkkkkasas dasdasdasda")))
